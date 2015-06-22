201954ed63dbd9c79881b888e09a93bc9dced9a2
Jim D <heyjim89@gmail.com>
2015-06-22 16:50:17 -0600

    Init commit

--- main.js
+++ main.js
@@ -1,43 +1,108 @@
 
-var phone = prompt("Enter your phone number XXX-XXX-XXXX");
-if ( (phone.charAt(3) === '-') && (phone.charAt(7) == '-' ) ) {
-	alert("correct");
-}
-else {
-	alert(" Phone naming schema incorrect. Please use XXX-XXX-XXXX ");
+// Init all arrays
+var victimName = [];
+var victimPhone = [];
+var victimStreet = [];
+
+var volunteerName = [];
+var volunteerPhone = [];
+var volunteerStreet = [];
+
+
+// Start Victim #
+var victimCount = prompt('Enter number of victims');
+
+
+// Start victim Prompts
+for(var i=0; i<victimCount; i++) {
+
+	victimName.push(prompt("Enter the victim's name:"));
+	victimPhone.push(prompt("Enter the victim's phone number:"));
+	victimStreet.push(prompt("Enter the victim's street:"));
 }
 
-var birth = prompt("Enter your birthdate XX/XX/XX");
-if ( (birth.charAt(2) === '/') && (birth.charAt(5) == '/' ) ) {
-	alert("correct");
-}
-else {
-	alert(" Naming schema incorrect. Please use XX/XX/XX ");
+// START VOLUNTEER PROMPTS
+var volunteerCount = prompt('How many volunteers?');
+
+for(var i=0; i<volunteerCount; i++) {
+
+	volunteerName.push(prompt("Enter the volunteer's name:"));
+	volunteerPhone.push(prompt("Enter the volunteer's phone number:"));
+	volunteerStreet.push(prompt("Enter the volunteer's street:"));
 }
 
-var postal = prompt("Enter your postal XXXXX or XXXXX-XXXX");
-if ( (postal.length === 5) || (postal.charAt(5) == '-' ) ) {
-	alert("correct");
-}
-else {
-	alert(" Naming schema incorrect. Please use XXXXX or XXXXX-XXXX");
+// Begin output section
+
+var output = "Victims: " + victimCount + "\n" + " Volunteers: " + volunteerCount + "\n";
+
+output += "____Victims:____\n";
+
+
+for(var i=0; i<victimCount; i++) {
+	output += "  Name: " + victimName[i] + ", " + "Phone: " + victimPhone[i] + ", " + "Street: " + victimStreet[i] + "\n";
 }
 
-var state = prompt("Enter your State (ex NJ or CO)");
-if ( (state.length === 2) && (state === state.toUpperCase() ) ) {
-	alert("correct");
-}
-else {
-	alert(" Naming schema incorrect. Please use XX ");
+output += "____Volunteers:____\n";
+
+for(var i=0; i<volunteerCount; i++) {
+	output += "  Name: " + volunteerName[i] + ", " + "Phone: " + volunteerPhone[i] + ", " + "Street: " + volunteerStreet[i] + "\n";
 }
 
-var married = prompt("Are you married?");
-if ( (married.toUpperCase() === "YES")) {
-	alert("correct");
-}
-else {
-	alert(" YAY ");
-}
+
+// Output all data
+alert(output);
+console.log(output);
+
+
+
+
+
+
+
+
+
+
+
+
+// var phone = prompt("Enter your phone number XXX-XXX-XXXX");
+// if ( (phone.charAt(3) === '-') && (phone.charAt(7) == '-' ) ) {
+// 	alert("correct");
+// }
+// else {
+// 	alert(" Phone naming schema incorrect. Please use XXX-XXX-XXXX ");
+// }
+
+// var birth = prompt("Enter your birthdate XX/XX/XX");
+// if ( (birth.charAt(2) === '/') && (birth.charAt(5) == '/' ) ) {
+// 	alert("correct");
+// }
+// else {
+// 	alert(" Naming schema incorrect. Please use XX/XX/XX ");
+// }
+
+// var postal = prompt("Enter your postal XXXXX or XXXXX-XXXX");
+// if ( (postal.length === 5) || (postal.charAt(5) == '-' ) ) {
+// 	alert("correct");
+// }
+// else {
+// 	alert(" Naming schema incorrect. Please use XXXXX or XXXXX-XXXX");
+// }
+
+// var state = prompt("Enter your State (ex NJ or CO)");
+// if ( (state.length === 2) && (state === state.toUpperCase() ) ) {
+// 	alert("correct");
+// }
+// else {
+// 	alert(" Naming schema incorrect. Please use XX ");
+// }
+
+// var married = prompt("Are you married?");
+// if ( (married.toUpperCase() === "YES")) {
+// 	alert("correct");
+// }
+// else {
+// 	alert(" YAY ");
+// }
 
 
 
@@ -77,7 +142,7 @@ else {
 
 // If you have gotten this far, what should you do? Commit!
 // Run git init to initialize the folder as a git repository.
-// Run git add -A and git commit -m "[ENTER MESSAGE]" to commit your changes to your local repo.
+// Run git add -A and git commit -m "[ENTER output]" to commit your changes to your local repo.
 // Create a repo on github using the instructions in How to Git. Push your changes to the remote github repo.
 // What the third character is (charAt)
 // The word in lowercase (toLowerCase)
